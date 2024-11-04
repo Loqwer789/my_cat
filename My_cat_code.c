a5ee4a9394d1443b4ac11583280bac2a207b65b9
Alexander <emtnp2005@gmail.com>
2024-11-04 18:20:25 +0300

    новая версия my cat

--- My_cat_code.c
+++ My_cat_code.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 struct s_flags
 {
 char flag;
@@ -10,7 +10,7 @@ char *m_flags;
 
 void append_flags(char *flags, char flag)
 {
-	struct s_flags a_flags[3] = { {"b","b"},{"n","n"},{"s","s"} };
+	struct s_flags a_flags[3] = { {'b',"b"},{'n',"n"},{'s',"s"} };
 
 	for( int i = 0; i < 3; i++)
 		{
@@ -18,57 +18,62 @@ void append_flags(char *flags, char flag)
 			{
 				for (size_t j = 0; j < strlen(a_flags[i].m_flags);j++)
 				{
-			append_flag(flags,a_flags[i].m_flags);
+			//append_flag(flags,a_flags[i].m_flags);
 			
 				}
 			break;	
 			}	
 		}	
 }
-
+/*
 void append_flag(char *flags, char flag)
 {
 
 
-if (strchr(flags,flag) == NULL)
+if (strchr(flags, flag) == NULL)
 	{
 		char tmp[2] = "-";
 		tmp[0] = flag;
 		strcat(flags,tmp);
 	}
 }	
-
+*/
 void flag_parser( char *flags, int argc, char **argv, int *idx)
 {
 for ( int i = 1; i < argc ; i++ )
 {
-	if (argv[i][0] != "-") 
+//	if (argv[i][0] != "-") 
 	{
 		break;
 	}	
-	else 
+//	else 
 	{
 	*idx = i;
 for (  size_t  j = 1;  j  < strlen(argv[i]) ; j++ )
 		{
-			append_flags(flags, argv[i][j]); 	
+//			append_flags(flags, argv[i][j]); 	
 		}	
 	}	
 }
 
 }
+typedef int bool;
 
-void print_file(char *name)
+int print_file(char *name,char *flags)
 {
 FILE *f = fopen(name,"rt");
 if(f != NULL) 
 {
+	int  index = 0;
+	bool printed = 0;
 	int c = fgetc(f);
+	
 	while (c != EOF)
-	{ 	
-		putc(c, stdout);
+	{
+	//	print_result(c, flags , index, printed); 	
+//		putc(c, stdout);
 		c = fgetc(f);
-		
+	
 	}
 
 fclose(f);
@@ -78,22 +83,37 @@ fclose(f);
 
 }	
 
+void print_result(int  c, char *flags, int *index, bool *printed)
+{
+//	if (!(strchr(flags, "s") != NULL ))
+			      // 	&& *p == "\n" && c == "\n" && *printed)) 
+	{
+	//	if(*p == "\n" && c == "\n" ) *printed = 1 else *printed = 0;
+	}	
+	
+	fputc(c,stdout);
+//	*p = c;
+
+
+
+}	
 
 int main( int argc, char  **argv)
 {	
 char flags[10] = "\0" ;
 int idx_end_flags = 1 ; 
 
-flag_parser(flags,argc,argv,idx_end_flags);
+flag_parser(flags, argc , argv, &idx_end_flags);
 
 for (int i = 1; i < argc; i++)
 	{
-	print_file(argv[i]);
+	print_file(argv[i], flags);
 	}	
 
 
 	return 0;
 }	
+
 
 
 
